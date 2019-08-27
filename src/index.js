94e49044ce727a87b8d4f8dcca9d6871726b6097
WashOliveira <washingtonfawkes@gmail.com>
2019-08-27 02:15:51 -0300

    Busca Funcionando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(
+	<App />
+, document.getElementById('root'));
 serviceWorker.unregister();
