de14f7b79213ef82d01bc08400d1f8b9706d8486
WashOliveira <washingtonfawkes@gmail.com>
2019-08-29 01:39:40 -0300

    Conexão com API que retorna Users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox'; 
+import './App.css';
 
 class App extends Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: '' 
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response=> response.json())
+			.then(users=>{this.setState({ robots: users})});
 	}
 
 	onSearchChange = (event) => {
@@ -20,13 +26,17 @@ class App extends Component {
 		const filteredRobots = this.state.robots.filter(robots =>{
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return(
-			<div className='tc'>
-				<h1>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
-			</div>
-		);
+		if(this.state.robots.length === 0){
+			return <h1>Loading</h1>
+		}else{
+			return(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={filteredRobots}/>
+				</div>
+			);
+		}
 	}
 }
 
